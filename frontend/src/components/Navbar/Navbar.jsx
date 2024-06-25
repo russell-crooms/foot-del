bb44f1736cadc7e7401eaec029df685fe645c9c4
Russell Crooms <russella.crooms@gmail.com>
2024-06-24 22:49:12 -0500

    Added our assets, and have begun building out the Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,26 @@
-import './Navbar.css';
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
 
 const Navbar = () => {
   return (
-    <div>
-        
+    <div className="navbar">
+      <img src={assets.logo} alt="Tomato." className="logo" />
+      <ul className="navbar-menu">
+        <li>home</li>
+        <li>menu</li>
+        <li>mobile app</li>
+        <li>contact us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="Search icon" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>sign in</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
