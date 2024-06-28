e97482fc1c390ee36acef8dd703280406ee2c7f0
Russell Crooms <russella.crooms@gmail.com>
2024-06-27 21:11:34 -0500

    Created the Header component, and mounted this to our app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Header />
     </div>
   )
 }
