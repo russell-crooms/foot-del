e6dd09450345df4f3ec1c42bbe2890b3643c6aea
Russell Crooms <russella.crooms@gmail.com>
2024-06-28 21:11:10 -0500

    Added styling to our Header - Completed this section of the project.

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="header">
       <div className="header-contents">
-        <h2>Order your favorite food here</h2>
+        <h2>Order your<br></br> favorite food here</h2>
         <p>
           Choose from a diverse menu featuring a delectable array of dishes
           crafted with the finest ingredients and culinary expertise. Our
