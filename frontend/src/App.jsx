4361b5d4c76f06f16d24444de188ab4eed016d3e
Russell Crooms <russella.crooms@gmail.com>
2024-06-28 21:40:38 -0500

    Added our Menu Items to the Explore Menu section. Will handle styling when we return.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
+import ExploreMenu from './components/ExploreMenu/ExploreMenu';
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
       <Header />
+      <ExploreMenu />
     </div>
   )
 }
